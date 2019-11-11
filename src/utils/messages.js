2a189818ef28d7c46e0b537f715ba98939ac2faa
Bulat <bulatxajrutdinoff@yandex.ru>
2019-11-11 11:22:23 +0300

    Реализовал функционал страницы Планирование и защитил роуты

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,5 +2,6 @@ export default {
   'logout': 'Вы вышли из системы',
   'auth/user-not-found': 'Пользователя с таким email не существует',
   'auth/wrong-password': 'Неверный email или пароль',
-  'auth/email-already-in-use': 'Данный email адрес уже зарегистрирован'
+  'auth/email-already-in-use': 'Данный email адрес уже зарегистрирован',
+  'login': 'Для начала войдите в систему'
 }
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,19 +2,25 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{info.bill | currencyFilter('RUB')}}</h4>
     </div>
 
-    <section>
-      <div>
-        <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+    <Loader v-if="loading" />
+    <p v-else-if="!categories.length">Категорий пока нет. <router-link to="/categories">Добавить новую категорию</router-link></p>
+    <section v-else>
+      <div
+          v-for="category in categories"
+          v-bind:key="category.id"
+      >
+        <p v-tooltip="category.tooltip">
+          <strong>{{category.name}}:</strong>
+          {{category.spend | currencyFilter('RUB')}} из {{category.limit | currencyFilter('RUB')}}
         </p>
         <div class="progress">
           <div
-              class="determinate green"
-              style="width:40%"
+              class="determinate"
+              v-bind:class="[category.progressColor]"
+              v-bind:style="{width:category.progressPercent + '%'}"
           ></div>
         </div>
       </div>
@@ -23,8 +29,40 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import currencyFilter from '@/filters/currency.filter'
 export default {
-  name: 'Planning'
+  name: 'Planning',
+  data: () => ({
+    loading: true,
+    categories: []
+  }),
+  methods: {},
+  async mounted () {
+    const records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
+    this.categories = categories.map(category => {
+      let spend = records
+        .filter(r => r.categoryId === category.id && r.type === 'outcome')
+        .reduce((sum, record) => { return sum += +record.amount }, 0)
+      let percent = spend * 100 / category.limit
+      let progressPercent = percent > 100 ? 100 : percent
+      let progressColor = percent < 60
+        ? 'green'
+        : percent < 100
+          ? 'yellow'
+          : 'red'
+
+      let tooltipValue = category.limit - spend
+      let tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${currencyFilter(Math.abs(tooltipValue))}`
+
+      return { ...category, progressPercent, progressColor, spend, percent, tooltip }
+    })
+    this.loading = false
+  },
+  computed: {
+    ...mapGetters(['info'])
+  }
 }
 </script>
 
